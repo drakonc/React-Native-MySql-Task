aceba8a4db2446053d94a9adc13af5af6d55675f
Jose Castro <31970492+drakonc@users.noreply.github.com>
2021-08-27 12:51:48 -0500

    ADD Logs

--- backend/src/App/app.js
+++ backend/src/App/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import fs from'fs';
+
+var log_file = fs.createWriteStream(__dirname + '/Logs/messages.log', {flags : 'a'});
 
 import tasksrouter from './Routers/task';
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('dev',{stream: log_file}));
 app.use(express.json());
 
 app.use(tasksrouter);
